4879d48f20aaeb4a11b2e9c36a6e6452a686f0d8
AycanM <aycanmintas34@gmail.com>
2019-05-07 02:02:45 +0300

    Check session variables on login page

--- src/KnowledgeCompetitionWebApp/Controllers/LoginController.cs
+++ src/KnowledgeCompetitionWebApp/Controllers/LoginController.cs
@@ -27,22 +27,35 @@ namespace KnowledgeCompetitionWebApp.Controllers
         {
             try
             {
-                if (!dbContext.Users.Any(u => u.Email.ToLower() == email.ToLower().Trim()))
-                    return Json(new { status = 0 });
-                if(dbContext.Users.Any(u => u.Email.ToLower() == email.ToLower().Trim() && u.Password != password))
-                    return Json(new { status = 2 });
-                var user = dbContext.Users.Where(u => u.Email == email && u.Password == password).FirstOrDefault();
-                Session["userType"] = user.UserType;
-                Session["userEmail"] = user.Email;
-                Session["userName"] = user.Name;
-                Session["userSurname"] = user.Surname;
-                return Json(
-                    new
-                    {
-                        status = 1,
-                        type = user.UserType
-                    }
-                );
+                if(Session["userType"] != null && Session["userEmail"] !=null && Session["userName"] != null && Session["userSurname"] != null)
+                {
+                    return Json(
+                        new
+                        {
+                            status = 1,
+                            type = Convert.ToInt16(Session["userType"])
+                        }
+                    );
+                }
+                else
+                {
+                    if (!dbContext.Users.Any(u => u.Email.ToLower() == email.ToLower().Trim()))
+                        return Json(new { status = 0 });
+                    if (dbContext.Users.Any(u => u.Email.ToLower() == email.ToLower().Trim() && u.Password != password))
+                        return Json(new { status = 2 });
+                    var user = dbContext.Users.Where(u => u.Email == email && u.Password == password).FirstOrDefault();
+                    Session["userType"] = user.UserType;
+                    Session["userEmail"] = user.Email;
+                    Session["userName"] = user.Name;
+                    Session["userSurname"] = user.Surname;
+                    return Json(
+                        new
+                        {
+                            status = 1,
+                            type = user.UserType
+                        }
+                    );
+                }
             }
             catch (Exception ex)
             {
